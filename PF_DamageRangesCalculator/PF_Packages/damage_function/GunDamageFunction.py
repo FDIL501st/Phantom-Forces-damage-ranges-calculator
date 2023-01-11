dd894126f45054da3a3f7c2b742d2660f759839d
FDIL <fdil.fadilh1@gmail.com>
2023-01-11 09:54:08 -0700

    Update GunDamageFunction.py
    Finished calculate_all_combinations_hits_to_kill() function.
    The other supporting functions will be finished next.

--- PF_DamageRangesCalculator/PF_Packages/damage_function/GunDamageFunction.py
+++ PF_DamageRangesCalculator/PF_Packages/damage_function/GunDamageFunction.py
@@ -10,6 +10,7 @@ class GunDamageOverRangeFunction(DamageFunction.DamageOverRangeFunction):
     """Represents damage over range function for guns."""
     def __init__(self, gun_dmg_info: GunDmgInf) -> None:
         super().__init__()
+        
         self.gun_damage_info = gun_dmg_info
         self.function_calculator = FunctionCalculator.DamageFunctionCalculator(gun_dmg_info, 100)
     
@@ -18,7 +19,14 @@ class GunDamageOverRangeFunction(DamageFunction.DamageOverRangeFunction):
         hits_to_kill: HitsToKill = {}
         if self.__gun_damage_info.torso_multi == 1.0:
             # Torso multi is 1, so can ignore torso hits
-            pass
+            hits_to_kill.update(self.__calculate_head_limb_hits)
+        else:
+            # Include torso hits
+            hits_to_kill.update(self.__calculate_head_torso_limb_hits)
+            hits_to_kill.update(self.__calculate_torso_limb_hits)
+
+        hits_to_kill.update(self.__calculate_only_limb_hits)
+        return hits_to_kill
 
     def __calculate_only_limb_hits(self) -> HitsToKill:
         """Calculates combinations of only limb hits that can kill."""

3241ee4b6370c422b637fdc4e6452a37ba3465fe
FDIL <fdil.fadilh1@gmail.com>
2023-01-13 10:19:14 -0700

    Implement code for calculating hits to kill of head+limb, and torso+limb and limb only.

--- PF_DamageRangesCalculator/PF_Packages/damage_function/GunDamageFunction.py
+++ PF_DamageRangesCalculator/PF_Packages/damage_function/GunDamageFunction.py
@@ -38,7 +38,7 @@ class GunDamageOverRangeFunction(DamageFunction.DamageOverRangeFunction):
 
         while True:
             hits: Hits = (0, 0, n_limb) # a potential hits to kil
-            range: float = self.function_calculator.calculate_max_range_hits_kill(hits)
+            range: float = self._function_calculator.calculate_max_range_hits_kill(hits)
             # Check if can kill
             if range != -1:
                 # Can kill, so add to hits_to_kill
@@ -59,13 +59,93 @@ class GunDamageOverRangeFunction(DamageFunction.DamageOverRangeFunction):
 
     def __calculate_torso_limb_hits(self) -> HitsToKill:
         """Calculates combinations of only torso and limb hits that can kill."""
+        hits_to_kill: HitsToKill = {}   # stores hits to kill
+        n_torso: int = 1     # stores number of torso shots
+        n_limb: int = 0      # stores number of limb shots
+
+        while True:
+            hits: Hits = (0, n_torso, n_limb)
+            range: float = self._function_calculator.calculate_max_range_hits_kill(hits)
+
+            #Check if can kill
+            if range != -1:
+                # can kill, so can add to hits_to_kill
+                hits_str: str = HitsToKillParser.convert_tuple_to_str(hits)
+                # This function only used when torso multi is not 1, so can assume that
+                hits_to_kill[hits_str] = range
+
+            # Check if can kill all range
+            if isinf(range):
+                # Check if have only torso hits (meaning n_limb == 0)
+                if n_limb == 0:
+                    # reached limit for hits,
+                    # adding more is redundant as already can kill all range with current number of torso hits
+                    # so can return hits_to_kill
+                    return hits_to_kill
+                
+                # We need to move to next torso shot and reset limb shot, then continue loop
+                n_torso += 1
+                n_limb = 0
+                continue
+            
+            # Need to add 1 limb shot for next iteration of loop
+            n_limb += 1
 
     def __calculate_head_limb_hits(self) -> HitsToKill:
         """Calculates combinations of only head and limb this that can kill."""
-    
+        hits_to_kill: HitsToKill = {}   # stores hits to kill
+        n_head: int = 1  # stores number of headshots
+        n_limb: int = 0  # stores number of limb shots
+
+        while True:
+            hits: Hits = (n_head, 0, n_limb)    # potential hits to kill
+            range: float = self._function_calculator.calculate_max_range_hits_kill(hits) 
+            # Check if can kill
+            if range != -1:
+                # can kill, so add to hits_to kill
+                hits_str: str = HitsToKillParser.convert_tuple_to_str(hits=hits, torso_multi_is_one=True)   
+                #This function only called when torso multi is 1
+                hits_to_kill[hits_str] = range
+
+            # Check if can kill all ranges
+            if isinf(range):
+                #Check if have only headshots (meaning n_limb == 0)
+                if n_limb == 0:
+                    # This means we reached our limit with hits, adding more is pointless and less hits will already kill all ranges
+                    # return hits_to_kill
+                    return hits_to_kill
+
+                # We need to move up 1 headshot and reset limb shots and continue loop
+                n_head += 1
+                n_limb = 0
+                continue
+            # Reaching here means need to add 1 limb shot for next iteration of loop
+            n_limb += 1
+
     def __calculate_head_torso_limb_hits(self) -> HitsToKill:
         """Calculates combinations of hits with atleast 1 headshot that can kill."""
+        hits_to_kill: HitsToKill = {}       # stores hits to kill
+        n_head: int = 1     # stores number of headshots
+        n_torso: int = 0    # stores number of torso shots
+        n_limb: int = 0     # stores number of limb shots
 
+        while True:
+            hits: Hits = (n_head, n_torso, n_limb)      # potential hits to kill
+            range: float = self._function_calculator.calculate_max_range_hits_kill(hits)
+
+            #Check if can kill
+            if range != -1:
+                # can kill, so can add to hits_to kill
+                hits_str: str = HitsToKillParser.convert_tuple_to_str(hits)
+                # This function only used when torso multi is not 1, so can assume that
+                hits_to_kill[hits_str] = range
+            
+            # Check if can kill all ranges
+            if isinf(range):
+                pass
+            
+            #Before next iteration of loop, need to add 1 limb shot
+            n_limb += 1
     # Getter and setters
     @property
     def gun_damage_info(self) -> GunDmgInf:

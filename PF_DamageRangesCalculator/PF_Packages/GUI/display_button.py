35ba00be524da6e3d317ed456a192a57f68a1f76
FDIL <fdil.fadilh1@gmail.com>
2023-01-27 08:38:18 -0700

    Update display_button.py
    Added a way for the children classes change their labels/text shown on the button during their constructor just by setting the label variable.

--- PF_DamageRangesCalculator/PF_Packages/GUI/display_button.py
+++ PF_DamageRangesCalculator/PF_Packages/GUI/display_button.py
@@ -1,15 +1,26 @@
 from typing import TypeAlias
 import tkinter
-from tkinter import ttk
+from tkinter import ttk, StringVar
 
 Button: TypeAlias = ttk.Button
 
 
+# noinspection SpellCheckingInspection
 class DisplayButton(Button):
     """The button from when pressed should do some calculations and display the results.
-    This class is the parent class of the button classes that will be actual buttons placed on the GUI.
+    This class is the parent class of the button classes
+    that will be actual buttons placed on the GUI.
     """
     def __init__(self, master: tkinter.Misc) -> None:
         super().__init__(master)
+
+        self.label: StringVar = StringVar()
+        # Variable to be set by children classes
+
+        self.config(textvariable=self.label)
+        # Set displayed text to label that will be set
+
+        # Using a StringVar and textvariable, so when children classes later sets the value of label,
+        # the value gets set automatically, and doesn't need to be specifically updated in children constructor
 
 

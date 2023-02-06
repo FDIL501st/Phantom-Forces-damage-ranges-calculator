c52457c334e71627adf95ba00aa459fa5cbbded0
FDIL <fdil.fadilh1@gmail.com>
2023-02-06 11:13:15 -0700

    Solved overwrite issue when save multiple windows at once
    Moved checks from constructor to save function.

--- PF_DamageRangesCalculator/PF_Packages/GUI/button/results_window/save_result.py
+++ PF_DamageRangesCalculator/PF_Packages/GUI/button/results_window/save_result.py
@@ -21,34 +21,28 @@ class SaveButton(Button):
         self.config(command=self.__save)
         self.__results: str = master.result_table.get_text()
 
-        # Issue with checking results here is if open multiple results windows,
-        # then press save on all of them, only 1 file gets saved, not all of them
-        # as the files in the directory at the creation of the windows don't have those save files
-
-        # This only work if open a new window after saving results from another window
-        self.__results_directory_path: str = "./Results"
-        # First check if Results directory exists
-        if not os.path.isdir(self.__results_directory_path):
-            # if it doesn't, create the directory
-            os.mkdir(self.__results_directory_path)
-        # At this point, Results directory exists
-
-        # find all files that already exist within directory
-        self.files: List[str] = os.listdir(self.__results_directory_path)
-
     def __save(self) -> None:
         """Creates a file and puts results in it.
         Files are saved in a directly called Results.
         Will not be replacing any previously created files.
         Nor will they be deleted.
         """
+        results_directory_path: str = "./Results"
+        # First check if Results directory exists
+        if not os.path.isdir(results_directory_path):
+            # if it doesn't, create the directory
+            os.mkdir(results_directory_path)
+        # At this point, Results directory exists
+
+        # find all files that already exist within directory
+        files: List[str] = os.listdir(results_directory_path)
 
         # do a loop to find a file name that doesn't exist
         i: int = 1
         filename: str = "result"
         while True:
-            potential_name: str = filename+str(i)+".txt"
-            if potential_name not in self.files:
+            potential_name: str = filename + str(i) + ".txt"
+            if potential_name not in files:
                 # found the name to use
                 filename = potential_name
                 break
@@ -57,7 +51,7 @@ class SaveButton(Button):
             i += 1
 
         # at this point, have a filename that doesn't exist right now
-        pathname: str = os.path.join(self.__results_directory_path, filename)
+        pathname: str = os.path.join(results_directory_path, filename)
 
         with open(pathname, 'w') as f:
             f.write(self.__results)

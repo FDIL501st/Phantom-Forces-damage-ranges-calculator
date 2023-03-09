3c219a6c1bcb5bb8a0d1daa008dfa18e03a5e5b6
FDIL <fdil.fadilh1@gmail.com>
2023-03-09 15:43:18 -0700

    Added widget in result window for user to type in filename
    Now user can type in filename for the name of the results file name.
    
    Currently need to fix up regex of save_result.py

---
+++ src/PF_Packages/GUI/button/results_window/filename_frame.py
@@ -0,0 +1,33 @@
+from typing import TypeAlias
+import tkinter
+from tkinter import ttk
+from . import result_window
+
+Frame: TypeAlias = ttk.Frame
+Label: TypeAlias = ttk.Label
+Entry: TypeAlias = ttk.Entry
+StringVar: TypeAlias = tkinter.StringVar
+ResultWindow: TypeAlias = 'result_window.ResultWindow'
+
+
+class FileNameFrame(Frame):
+    def __init__(self, master: ResultWindow) -> None:
+        super().__init__(master=master)
+
+        self.filename: StringVar = StringVar()  # saves the filename input by user
+
+        self.__createLabel()
+        self.__createEntry()
+
+    def __createLabel(self) -> None:
+        """Creates the label for the entry widget, telling user what they will input."""
+        label_text: str = "Enter name of text file\n to which results will be saved in"
+        # the label text is something to edit it enhance in the future
+
+        self.input_filename_label: Label = Label(master=self, text=label_text)
+        self.input_filename_label.pack(side=tkinter.LEFT)
+
+    def __createEntry(self) -> None:
+        """Create entry widget for user to write intput into."""
+        self.input_filename: Entry = Entry(master=self, textvariable=self.filename, width=50)
+        self.input_filename.pack(side=tkinter.RIGHT)
